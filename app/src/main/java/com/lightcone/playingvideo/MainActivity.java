e7756dad2db3a87f069b27a570ce08a673fb706d
guidrymwg <guidry@utk.edu>
2016-07-12 17:22:52 -0400

    cleanup

--- app/src/main/java/com/lightcone/playingvideo/MainActivity.java
+++ app/src/main/java/com/lightcone/playingvideo/MainActivity.java
@@ -1,12 +1,10 @@
 package com.lightcone.playingvideo;
 
-import android.graphics.Bitmap;
 import android.media.MediaMetadataRetriever;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.Menu;
 import java.io.File;
 
 import android.media.MediaPlayer;
